0498337135f28774f326d75b8de35d7b3585723e
Victor Hugo <victorhugoturnes@hotmail.com>
2023-03-20 17:56:52 -0300

    First attempt at conveyor integrations

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,24 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
+    id("dev.hydraulic.conveyor") version "1.4"
 }
 
 group = "br.com.selkeys"
 version = "1.0-SNAPSHOT"
 
+tasks.register<Exec>("convey") {
+    val app.site.host: String = "localhost"
+    val dir = layout.buildDirectory.dir("packages")
+    outputs.dir(dir)
+    commandLine("conveyor", "make", "--output-dir", dir.get(), "site")
+    dependsOn("jar", "writeConveyorConfig")
+}
+
 repositories {
     google()
     mavenCentral()
+    maven("https://jitpack.io")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
@@ -34,7 +44,7 @@ compose.desktop {
         mainClass = "MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "SelkeysControlSystem"
+            packageName = "Selkeys Control System"
             packageVersion = "1.0.0"
         }
     }
