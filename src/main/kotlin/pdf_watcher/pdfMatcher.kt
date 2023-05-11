94173ae7bc3c8bde28cd214ed2992d608021bfec
John Smith <selkeys11@hotmail.com>
2023-05-11 14:48:52 -0300

    Optimizing PDF matching, adding support for OFX matching
    Signed-off-by: John Smith <selkeys11@hotmail.com>

--- src/main/kotlin/pdf_watcher/pdfMatcher.kt
+++ src/main/kotlin/pdf_watcher/pdfMatcher.kt
@@ -2,6 +2,7 @@ package pdf_watcher
 
 import Preferences.monitoredFolder
 import Preferences.moveFiles
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import database.LocalDatabase
 import database.LocalDatabase.findAllDocuments
 import database.LocalDatabase.findTranslation
@@ -11,22 +12,38 @@ import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
 
-fun pdfToTxt(filePath: String) = PDDocument
-    .load(File(filePath))
-    .use { PDFTextStripper().getText(it).replace("\\s+".toRegex(), " ") }
+fun matchPdf(files: List<String>): List<Pair<String, String>> {
+    val documents = findAllDocuments()
+    return files
+        .map { file ->
+            try {
+                val txtFile = PDDocument
+                    .load(File(file))
+                    .use { PDFTextStripper().getText(it).replace("[\\s ]+".toRegex(), " ") }
+                Pair(file, documents.firstNotNullOfOrNull { buildDocumentName(it, txtFile) })
+            } catch (_: Exception) {
+                Pair("", null)
+            }
+        }
+        .mapNotNull { if (it.second != null) Pair(it.first, it.second!!) else null }
+}
 
-fun matchPdf(files: List<String>) = files
-    .filter { it.endsWith(".pdf") }
-    .map { file ->
-        val txtFile = pdfToTxt(file)
-        Pair(file, findAllDocuments().firstNotNullOfOrNull { buildDocumentName(it, txtFile) })
-    }
+fun matchOfx(files: List<String>): List<Pair<String, String>> {
+    val documents = findAllDocuments()
+    return files
+        .map { file ->
+            val txtFile = File(file).readText().replace("[\r\n\t ]+".toRegex(), " ")
+            Pair(file, documents.firstNotNullOfOrNull { buildDocumentName(it, txtFile) })
+        }
+        .mapNotNull { if (it.second != null) Pair(it.first, it.second!!) else null }
+}
 
 
 fun buildDocumentName(document: LocalDatabase.Document, file: String): String? {
     if (document.identifier.toRegex() !in file) return null
-    val registry = document.registryRegex.toRegex().find(file)?.value.orEmpty().replace("[^0-9]".toRegex(), "")
+    var registry = document.registryRegex.toRegex().find(file)?.value.orEmpty()
     val client = LocalDatabase.findByRegistry(registry)
+    registry = registry.replace("/".toRegex(), "_").replace("[^0-9_.]*".toRegex(), "")
     var filename = ""
     var folder = document.baseFolderStructure
     document.getProceduresOrdered().forEach {
@@ -55,19 +72,21 @@ fun matchZip(): Any {
 }
 
 fun processFiles(files: List<String>) {
-    val renamedPdf = matchPdf(files)
-    renamedPdf
-        .filter { it.second != null }
-        .forEach {
-            try {
-                if (moveFiles) {
-                    Files.createDirectories(Paths.get(File(it.second!!).parent))
-                    File(it.first).renameTo(File(it.second!!))
-                } else File(it.first).copyTo(File(it.second!!))
-            } catch (_: Exception) {
-                println(it.first)
-            }
-        }
-    val nameZip = matchZip()
-    val nameXml = matchXml()
+    matchPdf(files.filter { it.endsWith(".pdf") }).forEach(::moveOrCopy)
+    matchOfx(files.filter { it.endsWith(".ofx") }).forEach(::moveOrCopy)
+//    matchZip(files.filter { it.endsWith(".zip") })
+//    matchXml(files.filter { it.endsWith(".xml") })
+}
+
+private fun moveOrCopy(sourceDest: Pair<String, String>) {
+    val (source, destination) = sourceDest
+    try {
+        if (moveFiles) {
+            Files.createDirectories(Paths.get(File(destination).parent))
+            File(source).renameTo(File(destination))
+        } else File(source).copyTo(File(destination))
+    } catch (_: Exception) {
+        println("move or copy failed")
+        println("moving from $source to $destination")
+    }
 }
